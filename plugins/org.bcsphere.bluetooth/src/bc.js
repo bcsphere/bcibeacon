564ad0169da3c4d69cacd82cafd4eb97b4cd91de
lizhi <zhi.li@jumacc.com>
2014-04-21 15:35:40 +0800

    fix a bluetooth plugin bug.

--- plugins/org.bcsphere.bluetooth/src/bc.js
+++ plugins/org.bcsphere.bluetooth/src/bc.js
@@ -72,25 +72,27 @@
 	
 	//wait for plugin ready
 	var time = 0;
-	window.setTimeout(function(){
-		var interval = window.setInterval(function() {
-			var isAllReady = true;
-			for(var plugin in BC.plugins){
-				if(time == 5){
-					window.clearInterval(interval);
-					BC.bluetooth.dispatchEvent("pulginTimeout");
-				}else{
-					if(!plugin.isReady){
-						isAllReady = false;
+	document.addEventListener("bccoreready",function(){
+		window.setTimeout(function(){
+			var interval = window.setInterval(function() {
+				var isAllReady = true;
+				for(var plugin in BC.plugins){
+					if(time == 5){
+						window.clearInterval(interval);
+						BC.bluetooth.dispatchEvent("pulginTimeout");
+					}else{
+						if(!plugin.isReady){
+							isAllReady = false;
+						}
 					}
 				}
-			}
-			time++;
-			if(isAllReady){
-				fireDocumentEvent("bcready");
-			}
-		}, 100);
-	},100);
+				time++;
+				if(isAllReady){
+					fireDocumentEvent("bcready");
+				}
+			}, 100);
+		},100);
+	});
 	
 	//class extend function
 	var extend = function(protoProps, staticProps) {
